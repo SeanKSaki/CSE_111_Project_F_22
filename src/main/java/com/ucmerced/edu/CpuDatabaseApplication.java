25c57cd3c8f580ac2ca5d329135b463e429aef04
SeanKSaki <sksaki007@gmail.com>
2022-10-18 13:23:50 -0700

    inital commit

---
+++ src/main/java/com/ucmerced/edu/CpuDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.ucmerced.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucmerced/edu/CpuDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucmerced.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
